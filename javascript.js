db7bec951d2d3307f231fc6b72af85e1c3644930
Rodriguez365 <rmwila61@gmail.com>
2023-01-12 14:38:34 -0700

    Just did the most part of the project

---
+++ javascript.js
@@ -0,0 +1,58 @@
+const container = document.getElementById("grid_container")
+
+let new_color = "";
+const blue = document.getElementById("blue");
+blue.addEventListener("click", () => {
+    new_color = "blue";
+
+})
+const red = document.getElementById("red");
+red.addEventListener("click", () => {
+    new_color = "red";
+
+})
+
+function clear_grid() {
+    container.innerHTML = ""
+    update_grid();
+
+}
+
+
+function change() {
+    const choice = "blue,green,black,yellow,brown,red,orange,grey".split(",");
+    return choice[Math.floor(Math.random() * choice.length)];
+
+}
+const rainbow = document.getElementById("rainbow");
+rainbow.addEventListener("click", () => {
+    new_color = "rainbow";
+
+})
+
+
+function update_grid() {
+    range_value = document.querySelector("input").value;
+    let range_text = document.getElementById("range_text");
+    range_text.textContent = range_value + "x" + range_value;
+
+    container.style.gridTemplateColumns = `repeat(${range_value},1fr)`
+    container.style.gridTemplateRows = `repeat(${range_value}, 1fr)`
+
+    for (let i = 0; i < range_value * range_value; i++) {
+        const grid1 = document.createElement("div");
+        grid1.classList.add("grid_class")
+        grid1.addEventListener("click", function(e) {
+            if (new_color == "rainbow") {
+                e.target.style.backgroundColor = change();
+            } else {
+                e.target.style.backgroundColor = new_color;
+            }
+        });
+        container.appendChild(grid1)
+
+    }
+}
+window.onload = () => {
+    update_grid();
+}
